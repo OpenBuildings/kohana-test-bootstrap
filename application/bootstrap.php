e6fe8800233618404b2bb41dfce0936ec1e260ef
Haralan Dobrev <hkdobrev@gmail.com>
2016-07-28 13:00:58 +0300

    Remove obsolete welcome controller and route

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -119,13 +119,3 @@ Kohana::modules(array(
 	// 'unittest'   => MODPATH.'unittest',   // Unit testing
 	// 'userguide'  => MODPATH.'userguide',  // User guide and API documentation
 	));
-
-/**
- * Set the routes. Each route must have a minimum of a name, a URI and a set of
- * defaults for the URI.
- */
-Route::set('default', '(<controller>(/<action>(/<id>)))')
-	->defaults(array(
-		'controller' => 'welcome',
-		'action'     => 'index',
-	));
