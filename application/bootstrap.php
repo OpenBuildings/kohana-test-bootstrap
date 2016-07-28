aa0cebafc7b9ba43b8c9147efc556644a9091ca2
Haralan Dobrev <hkdobrev@gmail.com>
2016-07-28 13:04:22 +0300

    No need to check for a file which would always be missing

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -5,16 +5,8 @@
 // Load the core Kohana class
 require SYSPATH.'classes/Kohana/Core'.EXT;
 
-if (is_file(APPPATH.'classes/Kohana'.EXT))
-{
-	// Application extends the core
-	require APPPATH.'classes/Kohana'.EXT;
-}
-else
-{
-	// Load empty core extension
-	require SYSPATH.'classes/Kohana'.EXT;
-}
+// Load empty core extension
+require SYSPATH.'classes/Kohana'.EXT;
 
 /**
  * Set the default time zone.
